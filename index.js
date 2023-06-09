8902ccedbed826aac27284405c9f57c73a8b641e
Saiyam Jain <jimisierra2@gmail.com>
2023-06-09 22:38:28 +0530

    Counter

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let count = 0;
+function decrease(){
+    // counter.innerText
+    count = count-1;
+    document.getElementById("count").innerText = count;
+    console.log(count)
+}
+function increase(){
+    // counter.innerText
+    count = count+1;
+    document.getElementById("count").innerText = count;
+    console.log(count)
+}
